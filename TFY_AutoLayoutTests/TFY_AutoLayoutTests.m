928cc245df40f6d69a9c0f9f9832002f40a41f85
tianfengyou <420144542@qq.com>
2019-05-22 17:48:01 +0800

    Initial commit

---
+++ TFY_AutoLayoutTests/TFY_AutoLayoutTests.m
@@ -0,0 +1,37 @@
+//
+//  TFY_AutoLayoutTests.m
+//  TFY_AutoLayoutTests
+//
+//  Created by 田风有 on 2019/5/22.
+//  Copyright © 2019 恋机科技. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TFY_AutoLayoutTests : XCTestCase
+
+@end
+
+@implementation TFY_AutoLayoutTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
